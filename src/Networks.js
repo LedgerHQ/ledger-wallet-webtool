55b8badec6052dc475e6264a83f3bf6a0741f97f
amougel <alix.mougel@gmail.com>
2018-01-08 17:39:15 +0100

    prettier and proxy

--- src/Networks.js
+++ src/Networks.js
@@ -1,5 +1,7 @@
 const Coins = {
   0: {
+    apiName: "btc",
+    unit: "satoshi",
     name: "bitcoin",
     bip44: 0,
     p2pkh: 0,
@@ -12,13 +14,33 @@ const Coins = {
         private: 76066276,
         public: 76067358
       },
-      messagePrefix:"\x18Bitcoin Signed Message:\n",
+      messagePrefix: "\x18Bitcoin Signed Message:\n",
       pubKeyHash: 0,
       scriptHash: 5,
       wif: 128
     }
+  },
+  1: {
+    apiName: "btc_testnet",
+    unit: "satoshi",
+    name: "btc_testnet",
+    bip44: 1,
+    p2pkh: 111,
+    p2sh: 196,
+    familly: 1,
+    xpub: 0x043587cf,
+    bitcoinjs: {
+      bech32: "bc",
+      bip32: {
+        private: 70615956,
+        public: 70617039
+      },
+      messagePrefix: "\x18Bitcoin Signed Message:\n",
+      pubKeyHash: 111,
+      scriptHash: 196,
+      wif: 239
+    }
   }
-}
+};
 
-
-export default Coins
+export default Coins;
